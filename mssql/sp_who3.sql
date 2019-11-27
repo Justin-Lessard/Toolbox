8efbcfedf3672e5ab85a68ce4f6dce67c7c0074d
Justin Lessard <8126068+Justin-Lessard@users.noreply.github.com>
2019-11-27 11:32:29 -0500

    Create sp_who3.sql

---
+++ mssql/sp_who3.sql
@@ -0,0 +1,16 @@
+SELECT  
+  spid,
+  sp.[status],
+  loginame [Login],
+  hostname, 
+  blocked BlockedBy,
+  sd.name DBName, 
+  cmd Command,
+  cpu CPUTime,
+  physical_io DiskIO,
+  last_batch LastBatch,
+  [program_name] ProgramName   
+FROM
+  master.dbo.sysprocesses sp 
+  JOIN master.dbo.sysdatabases sd ON sp.dbid = sd.dbid
+ORDER BY spid 
