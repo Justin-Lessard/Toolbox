d64394e3a4663f1862efa9334dc00e191900421a
Justin Lessard <8126068+Justin-Lessard@users.noreply.github.com>
2021-05-17 11:38:57 -0400

    Update ZipTwoOrderedCollections.cs

--- .NET/Utils/ZipTwoOrderedCollections.cs
+++ .NET/Utils/ZipTwoOrderedCollections.cs
@@ -1,4 +1,4 @@
-var comparator = (l,r) => StringComparer.OrdinalIgnoreCase.Compare(l, r);
+int Comparator(string? l, string? r) => StringComparer.OrdinalIgnoreCase.Compare(l, r);
 using var leftEnumerator = someCollection.OrderBy(x => x.Key).GetEnumerator();
 using var rightEnumerator = otherCollection.OrderBy(x => x.Key).GetEnumerator();
 
@@ -8,20 +8,20 @@ var rightItem = rightEnumerator.MoveNext() ? rightEnumerator.Current : null;
 while (true)
 {
 	// Fast forward leftEnumerator if necessary
-	while (leftItem is not null && 0 > comparator(leftItem?.Key, rightItem?.Key)
+	while (leftItem is not null && 0 > comparator(leftItem.Key, rightItem?.Key))
 		leftItem = leftEnumerator.MoveNext() ? leftEnumerator.Current : null;
 
 	// Fast forward rightEnumerator if necessary
-	while (rightItem is not null && 0 < comparator(leftItem?.Key, rightItem?.Key)
+	while (rightItem is not null && 0 < comparator(leftItem?.Key, rightItem.Key))
 		rightItem = rightEnumerator.MoveNext() ? rightEnumerator.Current : null;
 
 	// Match between left and right
-	if (leftItem is not null && rightItem is not null && 0 == comparator(leftItem.Key, rightItem?.Key))
+	if (leftItem is not null && rightItem is not null && 0 == comparator(leftItem.Key, rightItem.Key))
 	{
 		yield return (leftItem, rightItem);
 		
 		leftItem = leftEnumerator.MoveNext() ? leftEnumerator.Current : null;
-		rightItem = otherList.MoveNext() ? otherList.Current : null;
+		rightItem = rightEnumerator.MoveNext() ? rightEnumerator.Current : null;
 		continue;
 	}
 	
