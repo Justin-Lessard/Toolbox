b0587c8e0e27ee38522707fb1543947e1ba73638
Justin Lessard <8126068+Justin-Lessard@users.noreply.github.com>
2021-05-17 14:35:05 -0400

    Update ZipTwoOrderedCollections.cs

--- .NET/Utils/ZipTwoOrderedCollections.cs
+++ .NET/Utils/ZipTwoOrderedCollections.cs
@@ -1,30 +1,53 @@
-int Comparator(string? l, string? r) => StringComparer.OrdinalIgnoreCase.Compare(l, r);
-using var leftEnumerator = someCollection.OrderBy(x => x.Key).GetEnumerator();
-using var rightEnumerator = otherCollection.OrderBy(x => x.Key).GetEnumerator();
+   /// <summary>
+        /// Zip two ordered list
+        /// </summary>
+        public IEnumerable<(TL left, TR right)> Zip<TL, TR>(IEnumerator<TL> leftEnumerator, IEnumerator<TR> rightEnumerator, Func<TL, TR, int> _comparator)
+            where TL : class
+            where TR : class
+        {
+            var leftItem = leftEnumerator.MoveNext() ? leftEnumerator.Current : null;
+            var rightItem = rightEnumerator.MoveNext() ? rightEnumerator.Current : null;
 
-var leftItem = leftEnumerator.MoveNext() ? leftEnumerator.Current : null;
-var rightItem = rightEnumerator.MoveNext() ? rightEnumerator.Current : null;
+            while (leftItem is not null && rightItem is not null)
+            {
+                // Fast forward leftEnumerator if necessary
+                while (leftItem is not null && 0 > _comparator(leftItem, rightItem))
+                    leftItem = leftEnumerator.MoveNext() ? leftEnumerator.Current : null;
+                if (leftItem is null)
+                    break;
 
-while (true)
-{
-	// Fast forward leftEnumerator if necessary
-	while (leftItem is not null && 0 > comparator(leftItem.Key, rightItem?.Key))
-		leftItem = leftEnumerator.MoveNext() ? leftEnumerator.Current : null;
+                // Fast forward rightEnumerator if necessary
+                while (rightItem is not null && 0 < _comparator(leftItem, rightItem))
+                    rightItem = rightEnumerator.MoveNext() ? rightEnumerator.Current : null;
+                if (rightItem is null)
+                    break;
 
-	// Fast forward rightEnumerator if necessary
-	while (rightItem is not null && 0 < comparator(leftItem?.Key, rightItem.Key))
-		rightItem = rightEnumerator.MoveNext() ? rightEnumerator.Current : null;
+                // Match between left and right
+                if (0 == _comparator(leftItem, rightItem))
+                {
+                    yield return (leftItem, rightItem);
+                    leftItem = leftEnumerator.MoveNext() ? leftEnumerator.Current : null;
+                    rightItem = rightEnumerator.MoveNext() ? rightEnumerator.Current : null;
+                }
+            }
+        }
 
-	// Match between left and right
-	if (leftItem is not null && rightItem is not null && 0 == comparator(leftItem.Key, rightItem.Key))
-	{
-		yield return (leftItem, rightItem);
-		
-		leftItem = leftEnumerator.MoveNext() ? leftEnumerator.Current : null;
-		rightItem = rightEnumerator.MoveNext() ? rightEnumerator.Current : null;
-		continue;
-	}
-	
-	if (leftItem is null || rightItem is null)
-		break;
-}
+
+
+
+
+
+
+        /// <summary>
+        /// Exemple usage
+        /// </summary>
+        private IEnumerable<PriceData> GetPriceDataEnumerable(IEnumerable<AxProductPrice> axData, ICollection<ProductCorrespondence> correspondencesCollection)
+        {
+            int Comparator(AxProductPrice l, ProductCorrespondence r) => StringComparer.OrdinalIgnoreCase.Compare(l.AxItemId, r.ExternalId);
+
+            using var leftEnumerator = axData.OrderBy(x => x.AxItemId).GetEnumerator();
+            using var rightEnumerator = correspondencesCollection.OrderBy(x => x.ExternalId).GetEnumerator();
+
+            return Zip(leftEnumerator, rightEnumerator, Comparator)
+                .Select(x => new PriceData(x.right.ProductId, x.left.Msrp, x.left.SalesPrice));
+        }
